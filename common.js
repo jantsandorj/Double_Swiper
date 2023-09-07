885efc9b4a2509d649fbd4381ca43c725e614ca3
jantsandorj <102676794+kieunMoon@users.noreply.github.com>
2023-09-07 13:37:17 +0800

    version 1

--- common.js
+++ common.js
@@ -1,23 +1,26 @@
 var swiperImg = new Swiper(".swiper1", {
   slidesPerView: 1,
   loop: true,
-  fade: true,
+  effect: "fade",
   crossFade: true,
-  mousewheel: { eventsTarget: ".swiper-container" },
+  mousewheel: { eventsTarget: ".projectWrap" },
   autoplay: {
-    delay: 4500,
+    delay: 100000,
     disableOnInteraction: false,
   },
 });
 var swiper = new Swiper(".swiper2", {
-  slidesPerView: 3,
+  slidesPerView: 2.5,
   direction: "vertical",
   loop: true,
-  spaceBetween: 30,
+  spaceBetween: 143,
   centeredSlides: true,
-  mousewheel: { eventsTarget: ".swiper-container" },
+  mousewheel: { eventsTarget: ".projectWrap" },
   autoplay: {
-    delay: 4500,
+    delay: 100000,
     disableOnInteraction: false,
   },
+});
+swiper.on("reachEnd", function () {
+  swiper.autoplay = false;
 });
